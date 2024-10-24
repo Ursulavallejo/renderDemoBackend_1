4a05813ca8358a4bf8e9346028d4a2e29db9cf02
Ursula Vallejo Janne <ursulavallejo@gmail.com>
2024-10-24 12:07:50 +0200

    remove cors

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express')
-const cors = require('cors')
 
 const app = express(),
   port = process.env.PORT || 3000
-
-app.use(cors())
 
 app.get('/', (_request, response) => {
   response.send({ hello: 'World' })
